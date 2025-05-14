a5d78de86883e6437d899a0f8b5fa9d8cc9b53b5
schan27 <schan27@users.noreply.github.com>
2025-05-14 14:27:58 -0400

    do not rank output twice

--- szeyap-api/src/szeyapapi/translation_logic/translator.py
+++ szeyap-api/src/szeyapapi/translation_logic/translator.py
@@ -78,7 +78,6 @@ class Translator:
             return response
         
         if q.lang == lang.EN:
-            answers = self.rank_by_fuzzy(q, answers)
             answers = self.rank_by_frequency(q, answers)
 
         for i, defn in enumerate(answers):
